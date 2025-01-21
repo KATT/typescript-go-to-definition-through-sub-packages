0068620f43764b7b3eec8bf23f3ede698426bcac
Alexander Johansson <alexander@n1s.se>
2025-01-21 01:58:22 +0100

    ok this is pretty ok

--- packages/a/src/index.ts
+++ packages/a/src/index.ts
@@ -6,6 +6,13 @@ export const a = t.router({
 	bar: proc.query(() => 'bar' as const),
 	baz: proc.query(() => 'baz' as const),
 	qux: proc.query(() => 'qux' as const),
+	sub: {
+		routers: {
+			are: {
+				cool: proc.query(() => 'cool' as const),
+			},
+		},
+	},
 });
 
 a.bar; // ✅ Go to definition works here
--- packages/b/src/index.ts
+++ packages/b/src/index.ts
@@ -1,6 +1,8 @@
 import { a } from '@org/a';
 import { t } from '@org/utils';
+import { createTRPCReact } from '@trpc/react-query';
 
+const aReact = createTRPCReact<typeof a>();
 a; // ✅ Go to definition works here with ^5.7.3
 a.foo; // 🚧 Go to definition does not work here, but seemingly fixed by https://github.com/microsoft/TypeScript/pull/60005
 // ^?
@@ -11,3 +13,7 @@ export const b = t.router({
 
 b.a.bar;
 b.a.foo;
+
+aReact.bar.useQuery();
+
+aReact.sub.routers.are.cool.useQuery();
